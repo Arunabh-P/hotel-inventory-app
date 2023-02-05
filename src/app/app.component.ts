9a1181d1a53cce434e9c71a1cde629309a1fe310
Arunabh-P <arunabh1995@gmail.com>
2023-02-05 19:26:11 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hinv-root',
+  templateUrl: './app.component.html',
+  // template: `<h1>template helloo</h1>
+  //   <p>how is he</p>`,
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'hotel-inventory-app';
+}
